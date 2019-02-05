99d176ca44def4b3336e7b0b546dab506701bdc1
David Hall <davidhallfreelancing@outlook.com>
2019-02-05 16:13:55 -0600

    Convert to MVC Design Pattern

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Unprivileged on 2/5/19.
+//  Copyright © 2019 Unprivileged. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
